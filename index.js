aca5d5177327529deb829466a3fee87622cba0a8
Prabin Parajuli <prabin.b9@gmail.com>
2020-10-29 17:42:42 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+ 1. Go through the full checkout process.
+ 2. Use getUserCredentials provided that returns the first name, last name and concats with email.  
+ 3. Write a function called getPreTaxTotal that take in our cartItems and returns the total without tax.
+ 4. Pass the preTaxTotal value to a getTaxedTotal function that returns the value with tax.
+ 5. Log each value from the 3 functions out. 
+*/
